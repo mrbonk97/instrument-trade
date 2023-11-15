9963ef12447431d9049bc151c4dfddf260613743
hyunsuk97 <mrbonk97@gmail.com>
2023-11-15 23:52:12 +0900

    first commit

---
+++ src/main/java/com/mrbonk/instrumenttrade/InstrumentTradeApplication.java
@@ -0,0 +1,13 @@
+package com.mrbonk.instrumenttrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstrumentTradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstrumentTradeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mrbonk/instrumenttrade/InstrumentTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrbonk.instrumenttrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstrumentTradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
